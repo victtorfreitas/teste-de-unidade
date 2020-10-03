7d9c2b3acdfdbfa6aadcf05fc9e46c797a8043fb
Victtor <victtorfreitas95@gmail.com>
2020-10-03 17:14:01 -0300

    Refes #6 Atualiza gerar pagamento

---
+++ src/br/com/caelum/leilao/infra/dao/Relogio.java
@@ -0,0 +1,7 @@
+package br.com.caelum.leilao.infra.dao;
+
+import java.util.Calendar;
+
+public interface Relogio {
+    Calendar hoje();
+}
--- src/br/com/caelum/leilao/servico/GeradorDePagamento.java
+++ src/br/com/caelum/leilao/servico/GeradorDePagamento.java
@@ -2,6 +2,7 @@ package br.com.caelum.leilao.servico;
 
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Pagamento;
+import br.com.caelum.leilao.infra.dao.Relogio;
 import br.com.caelum.leilao.infra.dao.RepositorioDeLeilao;
 import br.com.caelum.leilao.infra.dao.RepositorioDePagamentos;
 
@@ -13,13 +14,21 @@ public class GeradorDePagamento {
     private final RepositorioDePagamentos pagamentos;
     private final RepositorioDeLeilao leiloes;
     private final Avaliador avaliador;
+    private Relogio relogio;
+
+    public GeradorDePagamento(RepositorioDeLeilao leiloes,
+                              RepositorioDePagamentos pagamentos,
+                              Avaliador avaliador, Relogio relogio) {
+        this.leiloes = leiloes;
+        this.pagamentos = pagamentos;
+        this.avaliador = avaliador;
+        this.relogio = relogio;
+    }
 
     public GeradorDePagamento(RepositorioDeLeilao leiloes,
                               RepositorioDePagamentos pagamentos,
                               Avaliador avaliador) {
-        this.leiloes = leiloes;
-        this.pagamentos = pagamentos;
-        this.avaliador = avaliador;
+        this(leiloes, pagamentos, avaliador, new RelogioDoSistema());
     }
 
     public void gera() {
@@ -27,8 +36,20 @@ public class GeradorDePagamento {
         for (Leilao leilao : leiloesEncerrados) {
             avaliador.avalia(leilao);
 
-            Pagamento novoPagamento = new Pagamento(avaliador.getMaiorLance(), Calendar.getInstance());
+            Pagamento novoPagamento = new Pagamento(avaliador.getMaiorLance(), getPrimeiroDiaUtil());
             pagamentos.salva(novoPagamento);
         }
+    }
+
+    private Calendar getPrimeiroDiaUtil() {
+        Calendar data = relogio.hoje();
+        int diaDaSemana = data.get(Calendar.DAY_OF_WEEK);
+
+        if (diaDaSemana == Calendar.SATURDAY)
+            data.add(Calendar.DAY_OF_MONTH, 2);
+        else if (diaDaSemana == Calendar.SUNDAY)
+            data.add(Calendar.DAY_OF_MONTH, 1);
+
+        return data;
     }
 }
